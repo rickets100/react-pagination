556bb4ea0498957e57dc69394d5d62df4e47cbb7
Kay Krewson <kay.k@comcast.net>
2018-02-26 12:41:29 -0800

    added a placeholder for the navbar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,17 @@
 /* src/components/App.js */
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import Table from './table';
-import './app.scss';
-import './table.scss';
-import data from '../../bin/sample-data.js';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import Table from './table'
+import Navbar from './navbar'
+import './app.scss'
+import './table.scss'
+import './navbar.scss'
+import data from '../../bin/sample-data.js'
 
 class App extends Component {
   render() {
-    let myStr = "fred"
     return <div>
+            <Navbar />
             <Table
               data = {data}
               headers = {['First Name', 'Last Name', 'Country', 'Address', 'City', 'State', 'Zip', 'Phone']}
---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+import './navbar.scss'
+
+class Navbar extends Component {
+
+  render() {
+    console.log('\n^^^^^^^ IN NAVBAR RENDER ^^^^^^^');
+
+    return (
+      <div>
+        Hey there
+      </div>
+    )
+  }//render
+} //class Navbar
+
+
+export default Navbar;
